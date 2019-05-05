9afc05c7e40269e21cab8e538bcf25a8831d9ac1
MERGHOOB <margub.khan@gmail.com>
2019-05-05 20:05:34 +0530

    LC#8
    MY own Atoi

--- LeetCode/src/LC8_StringToInteger.java
+++ LeetCode/src/LC8_StringToInteger.java
@@ -1,20 +1,20 @@
 /**
  * Implement atoi which converts a string to an integer.
- *
+ * <p>
  * The function first discards as many whitespace characters as necessary until the first non-whitespace character is found.
  * Then, starting from this character, takes an optional initial plus or minus sign followed by as many numerical digits as possible, and interprets them as a numerical value.
- *
+ * <p>
  * The string can contain additional characters after those that form the integral number, which are ignored and have no effect on the behavior of this function.
- *
+ * <p>
  * If the first sequence of non-whitespace characters in str is not a valid integral number,
  * or if no such sequence exists because either str is empty or it contains only whitespace characters, no conversion is performed.
- *
+ * <p>
  * If no valid conversion could be performed, a zero value is returned.
- *
+ * <p>
  * Note:
- *
+ * <p>
  * Only the space character ' ' is considered as whitespace character.
- *
+ * <p>
  * Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−2^31,  2^31 − 1].
  * If the numerical value is out of the range of representable values, INT_MAX (231 − 1) or INT_MIN (−231) is returned.
  */
@@ -27,23 +27,54 @@ public class LC8_StringToInteger {
         int i = 0;
         StringBuilder result = new StringBuilder();
         boolean isNegative = false;
+        boolean isPositive = false;
         while (i < str.length()) {
 
             if (str.charAt(i) != ' ') {
+
+                // As we encounter a digit break the loop
+                if (Character.isDigit(str.charAt(i))) {
+                    break;
+                }
+                // if a non-space non-numerical character comes after '+', '-' return 0;
+                if (isNegative || isPositive) {
+                    return 0;
+                }
+                // if '-' encounterd, save this information, used at line#39 and #60
+                //and append it to result
                 if (str.charAt(i) == '-') {
                     result.append("-");
                     isNegative = true;
                     i++;
-                    break;
+                    continue;
                 }
-                if (Character.isDigit(str.charAt(i))) {
-                    break;
+                // if '-' encounterd, save this information, used at line#39 and #60
+                // no need to append
+                if (str.charAt(i) == '+') {
+                    isPositive = true;
+                    i++;
+                    continue;
                 }
+                // if non-digit character encounter before digit started return 0;
+                return 0;
+
+            }
+            // if after '+', '-' space is found return 0;
+            if (isPositive || isNegative) {
                 return 0;
             }
+
             i++;
         }
 
+        // if already we surpass the length, it means no digit is present, return 0;
+        if (i >= str.length()) {
+            return 0;
+        }
+
+
+        // It is sured that either '+' or '-' or nothing is present before a number.
+        // Following loop will stop if when non-numerical character encounterd
         while (i < str.length()) {
 
             if (!Character.isDigit(str.charAt(i))) {
@@ -53,13 +84,19 @@ public class LC8_StringToInteger {
             i++;
         }
 
-    return Integer.parseInt(result.toString());
+        try {
+            return Integer.parseInt(result.toString()); // result overflows or does not contain number format
+        } catch (NumberFormatException e) {
+            // result overflows or does not contain number format, return based on information of negativeness max or minvalue
+            return isNegative ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+        }
 
     }
 
+
     public static void main(String[] args) {
 
-        System.out.println(new LC8_StringToInteger().myAtoi("-42"));
+        System.out.println(new LC8_StringToInteger().myAtoi("143-"));
 
     }
 
