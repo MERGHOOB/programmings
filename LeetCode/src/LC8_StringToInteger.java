5d17c4eac3a7d48b2e6f95da3bd969c1f6001b34
MERGHOOB <margub.khan@gmail.com>
2019-05-05 18:58:48 +0530

    LC#8
    MY own Atoi

---
+++ LeetCode/src/LC8_StringToInteger.java
@@ -0,0 +1,67 @@
+/**
+ * Implement atoi which converts a string to an integer.
+ *
+ * The function first discards as many whitespace characters as necessary until the first non-whitespace character is found.
+ * Then, starting from this character, takes an optional initial plus or minus sign followed by as many numerical digits as possible, and interprets them as a numerical value.
+ *
+ * The string can contain additional characters after those that form the integral number, which are ignored and have no effect on the behavior of this function.
+ *
+ * If the first sequence of non-whitespace characters in str is not a valid integral number,
+ * or if no such sequence exists because either str is empty or it contains only whitespace characters, no conversion is performed.
+ *
+ * If no valid conversion could be performed, a zero value is returned.
+ *
+ * Note:
+ *
+ * Only the space character ' ' is considered as whitespace character.
+ *
+ * Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−2^31,  2^31 − 1].
+ * If the numerical value is out of the range of representable values, INT_MAX (231 − 1) or INT_MIN (−231) is returned.
+ */
+public class LC8_StringToInteger {
+
+    //Simple solution
+
+    public int myAtoi(String str) {
+
+        int i = 0;
+        StringBuilder result = new StringBuilder();
+        boolean isNegative = false;
+        while (i < str.length()) {
+
+            if (str.charAt(i) != ' ') {
+                if (str.charAt(i) == '-') {
+                    result.append("-");
+                    isNegative = true;
+                    i++;
+                    break;
+                }
+                if (Character.isDigit(str.charAt(i))) {
+                    break;
+                }
+                return 0;
+            }
+            i++;
+        }
+
+        while (i < str.length()) {
+
+            if (!Character.isDigit(str.charAt(i))) {
+                break;
+            }
+            result.append(str.charAt(i));
+            i++;
+        }
+
+    return Integer.parseInt(result.toString());
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(new LC8_StringToInteger().myAtoi("-42"));
+
+    }
+
+
+}
