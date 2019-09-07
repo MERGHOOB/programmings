dddea1e53903701ed1f9d64c0a2ee9daf2aefbea
MERGHOOB <margub.khan@gmail.com>
2019-09-07 23:52:13 +0530

    LC23 MergeKSortedList- recursively added by doing halves and merge them

--- LeetCode/src/linkedList/LC23_MergeKSortedList.java
+++ LeetCode/src/linkedList/LC23_MergeKSortedList.java
@@ -16,43 +16,99 @@ Output: 1->1->2->3->4->4->5->6
  */
 public class LC23_MergeKSortedList {
     //BruteForce
-    public static ListNode mergeKLists(ListNode[] lists) {
+//    public static ListNode mergeKLists(ListNode[] lists) {
+//
+//        if (lists.length == 1) {
+//            return lists[0];
+//        }
+//
+//
+//        ListNode listNode = new ListNode(Integer.MIN_VALUE);
+//        ListNode resultNode = listNode;
+//        boolean notToBreak = true;
+//        while (notToBreak) {
+//
+//            int minimumValue = Integer.MAX_VALUE;
+//            boolean allNull = true;
+//            int indexToBeIncrement = -1;
+//            for (int i = 0; i < lists.length; i++) {
+//                if (lists[i] != null) {
+//                    if (lists[i].val < minimumValue) {
+//                        indexToBeIncrement = i;
+//                        minimumValue = lists[i].val;
+//                    }
+//                    allNull = false;
+//                }
+//            }
+//
+//            if (allNull) {
+//                notToBreak = false;
+//            } else {
+//                listNode.next = lists[indexToBeIncrement];
+//                lists[indexToBeIncrement] = lists[indexToBeIncrement].next;
+//                listNode = listNode.next;
+//
+//            }
+//        }
+//
+//
+//        return resultNode.next;
+//    }
 
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0) {
+            return null;
+        }
         if (lists.length == 1) {
             return lists[0];
         }
 
+        return mergeKLists(lists, 0, lists.length - 1);
 
-        ListNode listNode = new ListNode(Integer.MIN_VALUE);
-        ListNode resultNode = listNode;
-        boolean notToBreak = true;
-        while (notToBreak) {
 
-            int minimumValue = Integer.MAX_VALUE;
-            boolean allNull = true;
-            int indexToBeIncrement = -1;
-            for (int i = 0; i < lists.length; i++) {
-                if (lists[i] != null) {
-                    if (lists[i].val < minimumValue) {
-                        indexToBeIncrement = i;
-                        minimumValue = lists[i].val;
-                    }
-                    allNull = false;
-                }
-            }
+    }
 
-            if (allNull) {
-                notToBreak = false;
-            } else {
-                listNode.next = lists[indexToBeIncrement];
-                lists[indexToBeIncrement] = lists[indexToBeIncrement].next;
-                listNode = listNode.next;
-
-            }
+    private ListNode mergeKLists(ListNode[] lists, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+        if (left == right) {
+            return lists[left];
+        }
+        if (left == right - 1) {
+            return mergeTwoLists(lists[left], lists[right]);
+        } else {
+            int mid = (left + right) / 2;
+            return mergeTwoLists(mergeKLists(lists, left, mid), mergeKLists(lists, mid + 1, right));
         }
 
+    }
 
-        return resultNode.next;
+    private ListNode mergeTwoLists(ListNode first, ListNode second) {
+
+        ListNode mergedTwoList = new ListNode(Integer.MIN_VALUE);
+        ListNode head = mergedTwoList;
+
+        while (first != null && second != null) {
+            if (first.val < second.val) {
+                mergedTwoList.next = first;
+                first = first.next;
+            } else {
+                mergedTwoList.next = second;
+                second = second.next;
+            }
+
+            mergedTwoList = mergedTwoList.next;
+        }
+
+        if (first != null) {
+            mergedTwoList.next = first;
+        } else {
+            mergedTwoList.next = second;
+        }
+
+        return head.next;
     }
 
 
@@ -73,9 +129,9 @@ public class LC23_MergeKSortedList {
         lists[1] = second;
         lists[2] = third;
 
-        ListNode listNode = mergeKLists(lists);
-        assert listNode != null;
+        ListNode listNode = new LC23_MergeKSortedList().mergeKLists(lists);
         listNode.print();
+
 
     }
 
