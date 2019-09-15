bcc9992601223560dc7e3487deda41b455575284
MERGHOOB <margub.khan@gmail.com>
2019-09-15 18:52:53 +0530

    Tech Delight: Shortest Common Super Sequence

---
+++ DynamicProgramming/src/techDelight/ShortestCommonSuperSequence.java
@@ -0,0 +1,65 @@
+package techDelight;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ShortestCommonSuperSequence {
+
+    public static void main(String[] args) {
+        String first = "ABCBDAB";
+        String second = "BDCABA";
+
+        System.out.println(new ShortestCommonSuperSequence().scsLength(first, second));
+
+
+    }
+
+    private int scsLength(String first, String second) {
+        Map<String, Integer> lookup = new HashMap<>();
+//        return scsLength(first, first.length(), second, second.length(), lookup);
+        return scsLengthBottomUp(first, first.length(), second, second.length());
+    }
+
+    private int scsLength(String first, int m, String second, int n, Map<String, Integer> lookup) {
+        if (m == 0 || n == 0) {
+            return n + m;
+        }
+
+        String key = m + "|" + n;
+
+        if (lookup.get(key) == null) {
+            if (first.charAt(m - 1) == second.charAt(n - 1)) {
+                lookup.put(key, scsLength(first, m - 1, second, n - 1, lookup) + 1);
+            } else {
+                lookup.put(key, Integer.min(scsLength(first, m - 1, second, n, lookup) + 1,
+                        scsLength(first, m, second, n - 1, lookup) + 1));
+            }
+        }
+        return lookup.get(key);
+    }
+
+    //With bottom-up
+    private int scsLengthBottomUp(String first, int m, String second, int n) {
+
+
+       int [][] lookup = new int[m+1][n+1];
+       for (int i = 0; i<=m; i++) {
+
+           for(int j = 0; j<=n; j++) {
+               if(i == 0 || j==0) {
+                   lookup[i][j] = i+j;
+                   continue;
+               }
+
+               if(first.charAt(i-1) == second.charAt(j-1)) {
+                   lookup[i][j] = lookup[i-1][j-1]+1;
+               }
+               else {
+                   lookup[i][j] = Integer.min(lookup[i-1][j] +1, lookup[i][j-1]+1);
+               }
+           }
+       }
+        return lookup[m][n];
+    }
+
+}
