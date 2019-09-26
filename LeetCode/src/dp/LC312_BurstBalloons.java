041c64e59a20774e7d52deff3f77ee586d7eeee2
mekhan <merghoob_khan@mentor.com>
2019-09-26 10:49:19 +0530

    MatrixChainMultiplication.java LC312_BurstBalloons.java

--- LeetCode/src/dp/LC312_BurstBalloons.java
+++ LeetCode/src/dp/LC312_BurstBalloons.java
@@ -16,6 +16,31 @@ time per subproblem will be O(j-i) = O(n);
 
 
  */
+/*
+o(N ^ 3) time
+o(N ^ 2) space
+1. define subproblems
+dp[i][j] : the maximum coins we can collect by bursting all the balloons in nums[i:j+1]
+number of subproblems o(N ^ 2)
+2. guess
+What is the last balloon to burst in nums[i:j+1]
+number of choices o(j - i + 1) or o(N)
+3. relate subproblem solutions
+i - 1, i, i + 1, ... , k - 1, k, k + 1, ..., j - 1, j, j + 1
+       ---------------------     --------------------
+              dp[i][k - 1]            dp[k + 1][j]
+
+dp[i][j] = max(dp[i][k - 1] + nums[i - 1] * nums[k] * nums[j + 1] + dp[k + 1][j])  i <= k <= j
+corner case: i == k == j, dp[i][i - 1] and dp[k + 1][j] initialized to zero --> same equation
+time per subproblem o(j - i) or O(N)
+4. topological order
+length from 1 to n
+5. original problem
+1, X, ..., X    , X, 1      nums
+0, 1, ..., n - 1, n, n + 1  index
+   ----------------
+      dp[1][n]
+*/
 
 public class LC312_BurstBalloons {
 
