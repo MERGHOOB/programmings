33d74a02553ab635e8e3f3fbe2e0244e61b85e32
mekhan <merghoob_khan@mentor.com>
2019-10-15 11:27:37 +0530

    LC_647_PalindromicSubString.java: expand around center

--- LeetCode/src/string/LC_647_PalindromicSubString.java
+++ LeetCode/src/string/LC_647_PalindromicSubString.java
@@ -11,7 +11,10 @@ expandAroundCenter:
     a|a|a: , total values will become 2*n -1;
 
     consider 0th center: left = 0; right = 0+0 = 0;
-    consider 1'st center: left = 0; right = 0+1;
+    consider 1'st center: left = 0; right = 0+1 = 1;
+    consider 2'st center: left = 1; right = 1+0 = 1;
+    consider 3'st center: left = 1; right = 1+1 = 2; // center here is virtual, it will be even length pallindrom
+    consider 4'st center: left = 2; right = 2+0 = 2; // center here is index2, it will be odd length
  */
 
     public static int countSubstrings1(String s) {
